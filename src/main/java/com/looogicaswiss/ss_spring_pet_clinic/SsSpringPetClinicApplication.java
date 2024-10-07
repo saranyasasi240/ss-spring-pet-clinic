9f1cf29e9a871828b4137c6838546a6c63dbf24d
Prasad <saranyasasi240@gmail.com>
2024-10-07 12:46:11 +0530

    Project Initial set up

---
+++ src/main/java/com/looogicaswiss/ss_spring_pet_clinic/SsSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.looogicaswiss.ss_spring_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsSpringPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsSpringPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/looogicaswiss/ss_spring_pet_clinic/SsSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.looogicaswiss.ss_spring_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsSpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
