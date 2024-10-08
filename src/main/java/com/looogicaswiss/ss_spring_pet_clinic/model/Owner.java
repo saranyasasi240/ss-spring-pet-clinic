970d78f3f8bce232c5ca0fbeb3b9d881c73dec4f
Prasad <saranyasasi240@gmail.com>
2024-10-08 17:12:22 +0530

    Added POJOs

---
+++ src/main/java/com/looogicaswiss/ss_spring_pet_clinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.looogicaswiss.ss_spring_pet_clinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/looogicaswiss/ss_spring_pet_clinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.looogicaswiss.ss_spring_pet_clinic.model;
+
+public class Vet extends Person {
+}
