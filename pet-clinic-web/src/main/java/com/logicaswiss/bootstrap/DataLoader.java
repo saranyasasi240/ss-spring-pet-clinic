1653b2eb673e20d756702a18e7f6fb05c4690fb1
Prasad <saranyasasi240@gmail.com>
2024-10-14 15:20:01 +0530

    Added PetType to bootstrap

--- pet-clinic-web/src/main/java/com/logicaswiss/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/logicaswiss/bootstrap/DataLoader.java
@@ -1,8 +1,11 @@
 package com.logicaswiss.bootstrap;
 
 import com.logicaswiss.model.Owner;
+import com.logicaswiss.model.PetType;
 import com.logicaswiss.model.Vet;
 import com.logicaswiss.services.OwnerService;
+import com.logicaswiss.services.PetService;
+import com.logicaswiss.services.PetTypeService;
 import com.logicaswiss.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +15,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner = new Owner();
         owner.setFirstName("John");
         owner.setLastName("Doe");
