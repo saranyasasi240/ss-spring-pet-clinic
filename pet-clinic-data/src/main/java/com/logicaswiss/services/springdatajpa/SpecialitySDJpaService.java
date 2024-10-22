2ded50c46344022ca8692f37085f07d1d1238cfa
Prasad <saranyasasi240@gmail.com>
2024-10-22 20:00:03 +0530

    Added spring data jpa for Speciality, Refactored.

---
+++ pet-clinic-data/src/main/java/com/logicaswiss/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,47 @@
+package com.logicaswiss.services.springdatajpa;
+
+import com.logicaswiss.model.Speciality;
+import com.logicaswiss.repositories.SpecialityRepository;
+import com.logicaswiss.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialityService {
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return specialityRepository.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        specialityRepository.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
