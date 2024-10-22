f0bbf43549825878c7382d17d1672ccb65d72991
Prasad <saranyasasi240@gmail.com>
2024-10-22 19:36:49 +0530

    Added spring data jpa for Vet

--- pet-clinic-data/src/main/java/com/logicaswiss/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/com/logicaswiss/services/springdatajpa/OwnerSDJpaService.java
@@ -5,12 +5,14 @@ import com.logicaswiss.repositories.OwnerRepository;
 import com.logicaswiss.repositories.PetRepository;
 import com.logicaswiss.repositories.PetTypeRepository;
 import com.logicaswiss.services.OwnerService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Profile("springdatajpa")
 public class OwnerSDJpaService implements OwnerService {
 
     private final OwnerRepository ownerRepository;
---
+++ pet-clinic-data/src/main/java/com/logicaswiss/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,47 @@
+package com.logicaswiss.services.springdatajpa;
+
+import com.logicaswiss.model.Vet;
+import com.logicaswiss.repositories.VetRepository;
+import com.logicaswiss.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
