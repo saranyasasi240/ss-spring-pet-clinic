bd244d1dfe0350896b09ffd021c78c4eea181208
Prasad <saranyasasi240@gmail.com>
2024-10-23 09:48:51 +0530

    Added spring data jpa for Visit. Added visit in data loader

--- pet-clinic-web/src/main/java/com/logicaswiss/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/logicaswiss/bootstrap/DataLoader.java
@@ -1,12 +1,11 @@
 package com.logicaswiss.bootstrap;
 
-import com.logicaswiss.model.Owner;
-import com.logicaswiss.model.PetType;
-import com.logicaswiss.model.Speciality;
-import com.logicaswiss.model.Vet;
+import com.logicaswiss.model.*;
 import com.logicaswiss.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -15,13 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialityService specialityService) {
+                      SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -56,14 +57,41 @@ public class DataLoader implements CommandLineRunner {
         Owner owner = new Owner();
         owner.setFirstName("John");
         owner.setLastName("Doe");
+        owner.setAddress("123 Brickerel");
+        owner.setCity("Miami");
+        owner.setTelephone("1231231234");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner.getPets().add(mikesPet);
 
         ownerService.save(owner);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Alex");
         owner1.setLastName("Manual");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("1231231234");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner1);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner1.getPets().add(fionasCat);
 
         ownerService.save(owner1);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners.....");
 
