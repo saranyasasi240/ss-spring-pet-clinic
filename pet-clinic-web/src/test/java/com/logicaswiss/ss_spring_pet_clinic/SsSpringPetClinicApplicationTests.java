8ee36c35b9bb780108d9086bb6611ebeec372af8
Prasad <saranyasasi240@gmail.com>
2024-10-24 22:05:57 +0530

    Added maven surefire plugin | Added @ExtendWith

--- pet-clinic-web/src/test/java/com/logicaswiss/ss_spring_pet_clinic/SsSpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/logicaswiss/ss_spring_pet_clinic/SsSpringPetClinicApplicationTests.java
@@ -1,10 +1,11 @@
 package com.logicaswiss.ss_spring_pet_clinic;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@Disabled
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SsSpringPetClinicApplicationTests {
 
