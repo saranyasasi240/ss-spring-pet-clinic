f8300ec0444f2e4dae13ca2e917895921f5985cc
Prasad <saranyasasi240@gmail.com>
2024-10-25 12:27:53 +0530

    Added CRUD test for OwnerMapService

--- pet-clinic-data/src/main/java/com/logicaswiss/model/Owner.java
+++ pet-clinic-data/src/main/java/com/logicaswiss/model/Owner.java
@@ -12,17 +12,21 @@ import java.util.Set;
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
-
     @Builder
-    public Owner(String address, String city, String telephone, Set<Pet> pets) {
+    public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
         this.address = address;
         this.city = city;
         this.telephone = telephone;
         this.pets = pets;
+
+        if (pets != null) {
+            this.pets = pets;
+        }
     }
 
     @Column(name = "address")
-    private  String address;
+    private String address;
 
     @Column(name = "city")
     private String city;
