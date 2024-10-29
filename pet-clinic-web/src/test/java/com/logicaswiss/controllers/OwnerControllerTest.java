5f604fc5c198746b89a9ac0df6fa065f4b3c7b20
Prasad <saranyasasi240@gmail.com>
2024-10-29 16:35:47 +0530

    Implemented Owner display

--- pet-clinic-web/src/test/java/com/logicaswiss/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/logicaswiss/controllers/OwnerControllerTest.java
@@ -14,7 +14,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -49,7 +51,7 @@ class OwnerControllerTest {
         mockMvc.perform(get("/owners"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/index"))
-                .andExpect(model().attribute("owners",hasSize(2)));
+                .andExpect(model().attribute("owners", hasSize(2)));
     }
 
     @Test
@@ -58,7 +60,7 @@ class OwnerControllerTest {
         mockMvc.perform(get("/owners/index"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/index"))
-                .andExpect(model().attribute("owners",hasSize(2)));
+                .andExpect(model().attribute("owners", hasSize(2)));
     }
 
     @Test
@@ -67,5 +69,14 @@ class OwnerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("notimplemented"));
         verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
