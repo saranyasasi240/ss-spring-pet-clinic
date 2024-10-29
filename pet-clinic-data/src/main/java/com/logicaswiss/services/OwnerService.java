b2f95ee194df3a2b95bc48140ccd8916a184b1ee
Prasad <saranyasasi240@gmail.com>
2024-10-29 19:39:24 +0530

    Added code for finding an owner

--- pet-clinic-data/src/main/java/com/logicaswiss/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/logicaswiss/services/OwnerService.java
@@ -2,6 +2,9 @@ package com.logicaswiss.services;
 
 import com.logicaswiss.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
